f664e12502a9b3b757d8331b8d653ebab2d628d4
Ilay cohen <77060249+ilayc11@users.noreply.github.com>
2025-03-20 15:46:24 +0000

    added read word function and search function

--- readfile.sh
+++ readfile.sh
@@ -4,26 +4,40 @@ for line in $(cat $path); do
 	echo "$line"
 done
 }
-function secondfunc(){
 
-}
-while true; do
-	echo "enter path of file"
-	read path
-	if [[ ! -e "$path" ]]; then
-		echo "bad path, try again"
-		continue
+function secondfunc(){
+echo "number of words:" $(wc -w "$path" | awk '{print $1}')
+echo "number of lines:" $(wc -l "$path" | awk '{print $1}')
+echo "size of file:" $(wc -c "$path" | awk '{print $1}')
+if [[ "$1" != "" ]]; then
+	if grep -q "$term" $path;then
+		awk -v termToSearch="$term" '
+		{
+		for ( i = 1; 1 <= NF; i++) {
+			count++
+			if ($i == termToSearch) {
+				print "true, word num:", count
+			}
+		}
+		}
+		' "$path"
 	fi
-	echo "choose option:"
-	echo "1. print line by line"
-	echo "2. return word count, line number and total file size"
-	echo "3. search function"
-	read op
-		if [[ $op -eq 1 ]]; then
-		firstfunc
-		elif [[ $op -eq 2 ]]; then
-		secondfunc
-		else
-		thirdfunc
-		fi
+fi
+}
+
+while true; do
+echo "enter path of file"
+read path
+if [[ ! -e "$path" ]]; then
+	echo "bad path, try again"
+	continue
+else
+	break
+fi
 done
+firstfunc
+echo "enter term to search"
+read term
+secondfunc $term
+echo "no more functions"
+	
